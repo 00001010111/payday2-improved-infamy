6fd9b39c1e6c643f8aa3e120dab2a88812450a5a
computerscience <EliosseTV@Gmail.com>
2026-02-09 19:37:39 +0100

    #test

--- hooks/exp_handler.lua
+++ hooks/exp_handler.lua
@@ -4,6 +4,5 @@ Hooks:PostHook(ExperienceManager, "get_max_prestige_xp", hook_id, function()
 	if Application:digest_value(tweak_data.experience_manager.prestige_xp_max, false) then
 		return 5000000
 	end
-
 	return 0
 end)
